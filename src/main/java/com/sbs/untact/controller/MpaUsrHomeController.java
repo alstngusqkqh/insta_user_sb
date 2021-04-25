2c7d091a012197e837eba50727738ec9e5133652
alstngusqkqh <alstngusqkqh@gmail.com>
2021-04-25 15:00:31 +0900

    게시물 리스트 JSP 추가

--- src/main/java/com/sbs/untact/controller/MpaUsrHomeController.java
+++ src/main/java/com/sbs/untact/controller/MpaUsrHomeController.java
@@ -8,6 +8,12 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 public class MpaUsrHomeController {
+	
+	@RequestMapping("/")
+	public String showMainRoot() {
+		return "redirect:/mpaUsr/home/main";
+	}
+	
 	@RequestMapping("/mpaUsr/home/main")
 	public String showMain() {
 		return "mpaUsr/home/main";
